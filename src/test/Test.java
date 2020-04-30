49e71adfb492827f1f1f2abc4f88c52d353a1421
Thearchermancoding <owen.v.hayes@gmail.com>
2020-04-30 10:23:23 -0700

    Tested a few more cases.

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,14 +14,14 @@ public class Test {
         ArrayList<Element> elements = new ArrayList<Element>(3);
         elements.add(Element.C);
         elements.add(Element.H);
-        // elements.add(Element.N);
+        elements.add(Element.N);
 
         ArrayList<Double> compositions = new ArrayList<Double>(3);
         compositions.add(74.0);
         compositions.add(8.7);
         compositions.add(17.3);
-        int[] amounts = { 1, 1 };
-        Compound nicotine = new Compound(elements, amounts, 78.0);
+        int[] amounts = { 5, 7, 1 };
+        Compound nicotine = new Compound(elements, compositions, 521.0);
         System.out.println(nicotine);
     }
 }
