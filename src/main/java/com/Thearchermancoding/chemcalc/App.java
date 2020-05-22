58ab6daa15cc64e241d19120db1df46cba77d3b2
Owen Hayes <owen.v.hayes@gmail.com>
2020-05-21 18:14:21 -0700

    Figuring out JavaFX Stuff

--- src/main/java/com/Thearchermancoding/chemcalc/App.java
+++ src/main/java/com/Thearchermancoding/chemcalc/App.java
@@ -6,17 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The type App.
+ */
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("chemcalc.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../../../../resources/chemcalc.fxml"));
         primaryStage.setTitle("chemcalc");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/com/Thearchermancoding/chemcalc/Element.java
+++ src/main/java/com/Thearchermancoding/chemcalc/Element.java
@@ -7,243 +7,481 @@ import java.util.Map;
  * Enum holding data for the elements on the periodic table.
  */
 public enum Element {
-    /** Hydrogen. */
+    /**
+     * Hydrogen.
+     */
     H(1, "Hydrogen", 1.008),
-    /** Helium. */
+    /**
+     * Helium.
+     */
     He(2, "Helium", 4.0026022),
-    /** Lithium. */
+    /**
+     * Lithium.
+     */
     Li(3, "Lithium", 6.94),
-    /** Beryllium. */
+    /**
+     * Beryllium.
+     */
     Be(4, "Beryllium", 9.01218315),
-    /** Boron. */
+    /**
+     * Boron.
+     */
     B(5, "Boron", 10.81),
-    /** Carbon. */
+    /**
+     * Carbon.
+     */
     C(6, "Carbon", 12.011),
-    /** Nitrogen. */
+    /**
+     * Nitrogen.
+     */
     N(7, "Nitrogen", 14.007),
-    /** Oxygen. */
+    /**
+     * Oxygen.
+     */
     O(8, "Oxygen", 15.999),
-    /** Fluorine. */
+    /**
+     * Fluorine.
+     */
     F(9, "Fluorine", 18.9984031636),
-    /** Neon. */
+    /**
+     * Neon.
+     */
     Ne(10, "Neon", 20.17976),
-    /** Sodium. */
+    /**
+     * Sodium.
+     */
     Na(11, "Sodium", 22.989769282),
-    /** Magnesium. */
+    /**
+     * Magnesium.
+     */
     Mg(12, "Magnesium", 24.305),
-    /** Aluminium. */
+    /**
+     * Aluminium.
+     */
     Al(13, "Aluminium", 26.98153857),
-    /** Silicon. */
+    /**
+     * Silicon.
+     */
     Si(14, "Silicon", 28.085),
-    /** Phosphorus. */
+    /**
+     * Phosphorus.
+     */
     P(15, "Phosphorus", 30.9737619985),
-    /** Sulfur. */
+    /**
+     * Sulfur.
+     */
     S(16, "Sulfur", 32.06),
-    /** Chlorine. */
+    /**
+     * Chlorine.
+     */
     Cl(17, "Chlorine", 35.45),
-    /** Argon. */
+    /**
+     * Argon.
+     */
     Ar(18, "Argon", 39.9481),
-    /** Potassium. */
+    /**
+     * Potassium.
+     */
     K(19, "Potassium", 39.09831),
-    /** Calcium. */
+    /**
+     * Calcium.
+     */
     Ca(20, "Calcium", 40.0784),
-    /** Scandium. */
+    /**
+     * Scandium.
+     */
     Sc(21, "Scandium", 44.9559085),
-    /** Titanium. */
+    /**
+     * Titanium.
+     */
     Ti(22, "Titanium", 47.8671),
-    /** Vanadium. */
+    /**
+     * Vanadium.
+     */
     V(23, "Vanadium", 50.94151),
-    /** Chromium. */
+    /**
+     * Chromium.
+     */
     Cr(24, "Chromium", 51.99616),
-    /** Manganese. */
+    /**
+     * Manganese.
+     */
     Mn(25, "Manganese", 54.9380443),
-    /** Iron. */
+    /**
+     * Iron.
+     */
     Fe(26, "Iron", 55.8452),
-    /** Cobalt. */
+    /**
+     * Cobalt.
+     */
     Co(27, "Cobalt", 58.9331944),
-    /** Nickel. */
+    /**
+     * Nickel.
+     */
     Ni(28, "Nickel", 58.69344),
-    /** Copper. */
+    /**
+     * Copper.
+     */
     Cu(29, "Copper", 63.5463),
-    /** Zinc. */
+    /**
+     * Zinc.
+     */
     Zn(30, "Zinc", 65.382),
-    /** Gallium. */
+    /**
+     * Gallium.
+     */
     Ga(31, "Gallium", 69.7231),
-    /** Germanium. */
+    /**
+     * Germanium.
+     */
     Ge(32, "Germanium", 72.6308),
-    /** Arsenic. */
+    /**
+     * Arsenic.
+     */
     As(33, "Arsenic", 74.9215956),
-    /** Selenium. */
+    /**
+     * Selenium.
+     */
     Se(34, "Selenium", 78.9718),
-    /** Bromine. */
+    /**
+     * Bromine.
+     */
     Br(35, "Bromine", 79.904),
-    /** Krypton. */
+    /**
+     * Krypton.
+     */
     Kr(36, "Krypton", 83.7982),
-    /** Rubidium. */
+    /**
+     * Rubidium.
+     */
     Rb(37, "Rubidium", 85.46783),
-    /** Strontium. */
+    /**
+     * Strontium.
+     */
     Sr(38, "Strontium", 87.621),
-    /** Yttrium. */
+    /**
+     * Yttrium.
+     */
     Y(39, "Yttrium", 88.905842),
-    /** Zirconium. */
+    /**
+     * Zirconium.
+     */
     Zr(40, "Zirconium", 91.2242),
-    /** Niobium. */
+    /**
+     * Niobium.
+     */
     Nb(41, "Niobium", 92.906372),
-    /** Molybdenum. */
+    /**
+     * Molybdenum.
+     */
     Mo(42, "Molybdenum", 95.951),
-    /** Technetium. */
+    /**
+     * Technetium.
+     */
     Tc(43, "Technetium", 98),
-    /** Ruthenium. */
+    /**
+     * Ruthenium.
+     */
     Ru(44, "Ruthenium", 101.072),
-    /** Rhodium. */
+    /**
+     * Rhodium.
+     */
     Rh(45, "Rhodium", 102.905502),
-    /** Palladium. */
+    /**
+     * Palladium.
+     */
     Pd(46, "Palladium", 106.421),
-    /** Silver. */
+    /**
+     * Silver.
+     */
     Ag(47, "Silver", 107.86822),
-    /** Cadmium. */
+    /**
+     * Cadmium.
+     */
     Cd(48, "Cadmium", 112.4144),
-    /** Indium. */
+    /**
+     * Indium.
+     */
     In(49, "Indium", 114.8181),
-    /** Tin. */
+    /**
+     * Tin.
+     */
     Sn(50, "Tin", 118.7107),
-    /** Antimony. */
+    /**
+     * Antimony.
+     */
     Sb(51, "Antimony", 121.7601),
-    /** Tellurium. */
+    /**
+     * Tellurium.
+     */
     Te(52, "Tellurium", 127.603),
-    /** Iodine. */
+    /**
+     * Iodine.
+     */
     I(53, "Iodine", 126.904473),
-    /** Xenon. */
+    /**
+     * Xenon.
+     */
     Xe(54, "Xenon", 131.2936),
-    /** Cesium. */
+    /**
+     * Cesium.
+     */
     Cs(55, "Cesium", 132.905451966),
-    /** Barium. */
+    /**
+     * Barium.
+     */
     Ba(56, "Barium", 137.3277),
-    /** Lanthanum. */
+    /**
+     * Lanthanum.
+     */
     La(57, "Lanthanum", 138.905477),
-    /** Cerium. */
+    /**
+     * Cerium.
+     */
     Ce(58, "Cerium", 140.1161),
-    /** Praseodymium. */
+    /**
+     * Praseodymium.
+     */
     Pr(59, "Praseodymium", 140.907662),
-    /** Neodymium. */
+    /**
+     * Neodymium.
+     */
     Nd(60, "Neodymium", 144.2423),
-    /** Promethium. */
+    /**
+     * Promethium.
+     */
     Pm(61, "Promethium", 145),
-    /** Samarium. */
+    /**
+     * Samarium.
+     */
     Sm(62, "Samarium", 150.362),
-    /** Europium. */
+    /**
+     * Europium.
+     */
     Eu(63, "Europium", 151.9641),
-    /** Gadolinium. */
+    /**
+     * Gadolinium.
+     */
     Gd(64, "Gadolinium", 157.253),
-    /** Terbium. */
+    /**
+     * Terbium.
+     */
     Tb(65, "Terbium", 158.925352),
-    /** Dysprosium. */
+    /**
+     * Dysprosium.
+     */
     Dy(66, "Dysprosium", 162.5001),
-    /** Holmium. */
+    /**
+     * Holmium.
+     */
     Ho(67, "Holmium", 164.930332),
-    /** Erbium. */
+    /**
+     * Erbium.
+     */
     Er(68, "Erbium", 167.2593),
-    /** Thulium. */
+    /**
+     * Thulium.
+     */
     Tm(69, "Thulium", 168.934222),
-    /** Ytterbium. */
+    /**
+     * Ytterbium.
+     */
     Yb(70, "Ytterbium", 173.0451),
-    /** Lutetium. */
+    /**
+     * Lutetium.
+     */
     Lu(71, "Lutetium", 174.96681),
-    /** Hafnium. */
+    /**
+     * Hafnium.
+     */
     Hf(72, "Hafnium", 178.492),
-    /** Tantalum. */
+    /**
+     * Tantalum.
+     */
     Ta(73, "Tantalum", 180.947882),
-    /** Tungsten. */
+    /**
+     * Tungsten.
+     */
     W(74, "Tungsten", 183.841),
-    /** Rhenium. */
+    /**
+     * Rhenium.
+     */
     Re(75, "Rhenium", 186.2071),
-    /** Osmium. */
+    /**
+     * Osmium.
+     */
     Os(76, "Osmium", 190.233),
-    /** Iridium. */
+    /**
+     * Iridium.
+     */
     Ir(77, "Iridium", 192.2173),
-    /** Platinum. */
+    /**
+     * Platinum.
+     */
     Pt(78, "Platinum", 195.0849),
-    /** Gold. */
+    /**
+     * Gold.
+     */
     Au(79, "Gold", 196.9665695),
-    /** Mercury. */
+    /**
+     * Mercury.
+     */
     Hg(80, "Mercury", 200.5923),
-    /** Thallium. */
+    /**
+     * Thallium.
+     */
     Tl(81, "Thallium", 204.38),
-    /** Lead. */
+    /**
+     * Lead.
+     */
     Pb(82, "Lead", 207.21),
-    /** Bismuth. */
+    /**
+     * Bismuth.
+     */
     Bi(83, "Bismuth", 208.980401),
-    /** Polonium. */
+    /**
+     * Polonium.
+     */
     Po(84, "Polonium", 209),
-    /** Astatine. */
+    /**
+     * Astatine.
+     */
     At(85, "Astatine", 210),
-    /** Radon. */
+    /**
+     * Radon.
+     */
     Rn(86, "Radon", 222),
-    /** Francium. */
+    /**
+     * Francium.
+     */
     Fr(87, "Francium", 223),
-    /** Radium. */
+    /**
+     * Radium.
+     */
     Ra(88, "Radium", 226),
-    /** Actinium. */
+    /**
+     * Actinium.
+     */
     Ac(89, "Actinium", 227),
-    /** Thorium. */
+    /**
+     * Thorium.
+     */
     Th(90, "Thorium", 232.03774),
-    /** Protactinium. */
+    /**
+     * Protactinium.
+     */
     Pa(91, "Protactinium", 231.035882),
-    /** Uranium. */
+    /**
+     * Uranium.
+     */
     U(92, "Uranium", 238.028913),
-    /** Neptunium. */
+    /**
+     * Neptunium.
+     */
     Np(93, "Neptunium", 237),
-    /** Plutonium. */
+    /**
+     * Plutonium.
+     */
     Pu(94, "Plutonium", 244),
-    /** Americium. */
+    /**
+     * Americium.
+     */
     Am(95, "Americium", 243),
-    /** Curium. */
+    /**
+     * Curium.
+     */
     Cm(96, "Curium", 247),
-    /** Berkelium. */
+    /**
+     * Berkelium.
+     */
     Bk(97, "Berkelium", 247),
-    /** Californium. */
+    /**
+     * Californium.
+     */
     Cf(98, "Californium", 251),
-    /** Einsteinium. */
+    /**
+     * Einsteinium.
+     */
     Es(99, "Einsteinium", 252),
-    /** Fermium. */
+    /**
+     * Fermium.
+     */
     Fm(100, "Fermium", 257),
-    /** Mendelevium. */
+    /**
+     * Mendelevium.
+     */
     Md(101, "Mendelevium", 258),
-    /** Nobelium. */
+    /**
+     * Nobelium.
+     */
     No(102, "Nobelium", 259),
-    /** Lawrencium. */
+    /**
+     * Lawrencium.
+     */
     Lr(103, "Lawrencium", 266),
-    /** Rutherfordium. */
+    /**
+     * Rutherfordium.
+     */
     Rf(104, "Rutherfordium", 267),
-    /** Dubnium. */
+    /**
+     * Dubnium.
+     */
     Db(105, "Dubnium", 268),
-    /** Seaborgium. */
+    /**
+     * Seaborgium.
+     */
     Sg(106, "Seaborgium", 269),
-    /** Bohrium. */
+    /**
+     * Bohrium.
+     */
     Bh(107, "Bohrium", 270),
-    /** Hassium. */
+    /**
+     * Hassium.
+     */
     Hs(108, "Hassium", 269),
-    /** Meitnerium. */
+    /**
+     * Meitnerium.
+     */
     Mt(109, "Meitnerium", 278),
-    /** Darmstadtium. */
+    /**
+     * Darmstadtium.
+     */
     Ds(110, "Darmstadtium", 281),
-    /** Roentgenium. */
+    /**
+     * Roentgenium.
+     */
     Rg(111, "Roentgenium", 282),
-    /** Copernicium. */
+    /**
+     * Copernicium.
+     */
     Cn(112, "Copernicium", 285),
-    /** Nihonium. */
+    /**
+     * Nihonium.
+     */
     Nh(113, "Nihonium", 286),
-    /** Flerovium. */
+    /**
+     * Flerovium.
+     */
     Fl(114, "Flerovium", 289),
-    /** Moscovium. */
+    /**
+     * Moscovium.
+     */
     Mc(115, "Moscovium", 289),
-    /** Livermorium. */
+    /**
+     * Livermorium.
+     */
     Lv(116, "Livermorium", 293),
-    /** Tennessine. */
+    /**
+     * Tennessine.
+     */
     Ts(117, "Tennessine", 294),
-    /** Oganesson. */
+    /**
+     * Oganesson.
+     */
     Og(118, "Oganesson", 294),
-    /** Ununennium. */
+    /**
+     * Ununennium.
+     */
     Uue(119, "Ununennium", 315);
 
     private static class Holder {
@@ -270,42 +508,47 @@ public enum Element {
     }
 
     /**
+     * For atomic number element.
      *
-     * @param atomicNumber
-     * @return Element
+     * @param atomicNumber the atomic number
+     * @return Element element
      */
     public static Element forAtomicNumber(int atomicNumber) {
         return Holder.map.get(atomicNumber);
     }
 
     /**
+     * For name element.
      *
-     * @param name
-     * @return Element
+     * @param name the name
+     * @return Element element
      */
     public static Element forName(String name) {
         return Holder.map2.get(name);
     }
 
     /**
+     * Gets atomic number.
      *
-     * @return int
+     * @return int atomic number
      */
     public int getAtomicNumber() {
         return atomicNumber;
     }
 
     /**
+     * Gets full name.
      *
-     * @return String
+     * @return String full name
      */
     public String getFullName() {
         return fullName;
     }
 
     /**
+     * Gets atomic mass.
      *
-     * @return double
+     * @return double atomic mass
      */
     public double getAtomicMass() {
         return atomicMass;
@@ -322,8 +565,9 @@ public enum Element {
     }
 
     /**
+     * Gets info.
      *
-     * @return String
+     * @return String info
      */
     public String getInfo() {
         return "Name: " + getFullName()
